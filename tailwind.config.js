5a97ab291a2bb56f6ce459133a2f4b51807476a9
venkatesh c <venky26494@yahoo.com>
2022-09-23 23:02:08 +0530

    tiny cards-completed

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './public/*.html'
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        lg: '1124px',
+        xl: '1124px',
+        '2xl': '1124px',
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
